76465b429f27d24875c8b06adbc5942f8d6ca427
latypovroman <jiggawhat5@gmail.com>
2023-02-07 20:40:40 +0500

    refactor structure to FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import React, {Suspense} from 'react';
 import { Route, Routes, Link } from "react-router-dom";
-import "../styles/index.scss";
-import {AboutPageAsync} from "../pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "../pages/MainPage/MainPage.async";
-import {useTheme} from "../theme/useTheme";
-import {classNames} from "../helpers/classNames/classNames";
+import "./styles/index.scss";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 const App = () => {
     const { theme, switchTheme } = useTheme();
@@ -16,8 +16,8 @@ const App = () => {
             <button onClick={switchTheme}>Поменять тему</button>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path="/about" element={<AboutPageAsync/>}/>
-                    <Route path="/" element={<MainPageAsync/>}/>
+                    <Route path="/about" element={<AboutPage/>}/>
+                    <Route path="/" element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
