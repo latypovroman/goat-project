c8a5834a1f4be417ff9529eef1bbfb105a2dd557
latypovroman <jiggawhat5@gmail.com>
2023-02-09 17:16:47 +0500

    refactor classNames

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,9 +1,9 @@
 type Mods = Record<string, boolean | string>
 
-export function classNames( cls: string, mods: Mods, add: string[]): string {
+export function classNames( cls: string, mods: Mods = {}, add: string[] = []): string {
     return [
         cls,
-        ...add,
+        ...add.filter(Boolean),
         ...Object.entries(mods)
             .filter(([classname, value]) => !!value)
             .map(([classname, value]) => classname)
