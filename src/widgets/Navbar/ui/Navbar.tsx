a635bfc461715017004724cc5ebe5c8d1fd0d01e
latypovroman <jiggawhat5@gmail.com>
2023-02-11 14:06:47 +0500

    added Sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {classNames} from "shared/lib/classNames/classNames";
 import styles from "./Navabar.module.scss"
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
-import {ThemeSwitcher} from "widgets/ThemeSwitcher";
 
 interface NavbarProps {
     className?: string;
@@ -13,7 +12,6 @@ export const Navbar = (props: NavbarProps) => {
 
     return (
         <div className={classNames(styles.navbar, {}, [className])}>
-            <ThemeSwitcher />
             <div className={styles.links}>
                 <AppLink theme={AppLinkTheme.LIGHT} to="/">Главная</AppLink>
                 <AppLink theme={AppLinkTheme.LIGHT} to="/about">О сайте</AppLink>
